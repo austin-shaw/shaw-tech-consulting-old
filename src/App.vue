efd1f683aad9be2a317b19c3cc63c2ff259fbefe
Austin Shaw <austinshaw_@hotmail.com>
2018-12-20 13:03:08 -0800

    First commit, newly created project.
    Committing now so that it can be worked on easily from other machines.

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>TEST</span>
+        <span class="font-weight-light"> Tech Consulting</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/austin-shaw"
+        target="_blank"
+      >
+        <!-- <span class="mr-2">Latest Release</span> -->
+        <v-icon>fab fa-github</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
